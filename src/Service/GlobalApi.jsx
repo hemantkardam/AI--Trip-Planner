01a4c24a7675d8a859c95c137ee55b9dc3eddb55
hemantkardam <hemantkardam8287@gmail.com>
2025-01-25 00:36:36 +0530

    update

--- src/Service/GlobalApi.jsx
+++ src/Service/GlobalApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/places";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL + "/places";
 
 export const GetPlaceDetails = (data) => axios.post(BASE_URL, data);
 export const PHOTO_REF_URL =
